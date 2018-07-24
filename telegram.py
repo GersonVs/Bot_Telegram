07d7d04b8788f29bc343709ac560b68baea4fa80
Gerson Vieira <ichigokurosaki.ga65@gmail.com>
2018-07-24 20:29:17 -0300

    Add files via upload

---
+++ telegram.py
@@ -0,0 +1,22 @@
+import telepot
+from Chatbot import Chatbot
+
+telegram = telepot.Bot("Token")
+
+bot = Chatbot("VashEstouroDaBoiada")
+
+def recebendoMsg(msg):
+    frase = bot.escuta(frase=msg['text'])
+    resp = bot.pensa(frase)
+    bot.fala(resp)
+    #chatID = msg['chat']['id']
+    tipoMsg, tipoChat, chatID = telepot.glance(msg)
+    telegram.sendMessage(chatID,resp)
+
+telegram.message_loop(recebendoMsg)
+
+while True:
+    pass
+
+
+    
